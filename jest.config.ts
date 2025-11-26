24ced2664fba700bc56c63a0636ea781f720a16a
GP <gregpetropoulos@yahoo.com>
2025-11-25 23:27:31 -0500

    config files

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,8 @@ const config: Config = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
   ],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{ts,tsx,js,jsx}', '!**/coverage/**', '!**/node_modules/**', '!**/babel.config.js', '!**/expo-env.d.ts', '!**/.expo/**'],
 };
 
 export default config;
